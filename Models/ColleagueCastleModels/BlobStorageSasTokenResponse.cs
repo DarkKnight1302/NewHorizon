f8da0e4fdbc0f8993bc4aeffc88b90b5692be0e0
Robin Parashar <robin.parashar@yahoo.in>
2023-02-14 19:35:19 +0530

    Add blob storage response

---
+++ Models/ColleagueCastleModels/BlobStorageSasTokenResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace NewHorizon.Models.ColleagueCastleModels
+{
+    public class BlobStorageSasTokenResponse
+    {
+        public BlobStorageSasTokenResponse(string sasToken)
+        {
+            SasToken = sasToken;
+        }
+
+        public string SasToken { get; set; }
+    }
+}
