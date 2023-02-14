eb6d2ef8a3ef6685ab2447dcdab8936ad9b7e510
Robin Parashar <robin.parashar@yahoo.in>
2023-02-14 14:23:47 +0530

    Username availability API

---
+++ Controllers/UsernameController.cs
@@ -0,0 +1,36 @@
+﻿using GoogleApi.Entities.Places.Common;
+using Microsoft.AspNetCore.Mvc;
+using NewHorizon.Models.ColleagueCastleModels;
+using NewHorizon.Repositories.Interfaces;
+
+namespace NewHorizon.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsernameController : ControllerBase
+{
+    private readonly ILogger<TrafficDurationController> _logger;
+    private readonly IUserRepository userRepository;
+
+    public UsernameController(ILogger<TrafficDurationController> logger, IUserRepository userRepository)
+    {
+        _logger = logger;
+        this.userRepository = userRepository;
+    }
+
+    [ApiExplorerSettings(GroupName = "v1")]
+    [HttpGet("username-availability")]
+    public async Task<IActionResult> Get(string username) 
+    {
+        if (string.IsNullOrEmpty(username))
+        {
+            return BadRequest("Invalid Username");
+        }
+        User user = await this.userRepository.GetUserByUserNameAsync(username).ConfigureAwait(false);
+        if (user == null)
+        {
+            return Ok(new UsernameAvailabilityResponse(username, true));
+        }
+        return Ok(new UsernameAvailabilityResponse(username, false));
+    }
+}
---
+++ Models/ColleagueCastleModels/UsernameAvailabilityResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace NewHorizon.Models.ColleagueCastleModels
+{
+    public class UsernameAvailabilityResponse
+    {
+        public UsernameAvailabilityResponse(string username, bool isUsernameAvailable)
+        {
+            Username = username;
+            IsUsernameAvailable = isUsernameAvailable;
+        }
+
+        public string Username { get; set; }
+
+        public bool IsUsernameAvailable { get; set; }
+    }
+}
