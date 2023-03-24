135006ebfb2a9383523e9358b90d732a5f61bdf2
Shivam Pandey <pandeys@microsoft.com>
2023-03-24 17:26:39 +0530

    Reset Password controller

---
+++ Controllers/ResetPasswordController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewHorizon.Models.ColleagueCastleModels;
+using NewHorizon.Repositories.Interfaces;
+using NewHorizon.Utils;
+
+namespace NewHorizon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResetPasswordController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public ResetPasswordController(IUserRepository userRepository)
+        {
+            this._userRepository = userRepository;
+        }
+
+        [ApiKeyRequired]
+        [ApiExplorerSettings(GroupName = "v1")]
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordRequest resetPasswordRequest)
+        {
+            var userId = HttpContext.Request.Headers["X-Api-Key"];
+            if (string.IsNullOrEmpty(userId) || resetPasswordRequest.Username != userId)
+            {
+                return BadRequest("Invalid Username.");
+            }
+
+            var user = await _userRepository.GetUserByUserNameAsync(resetPasswordRequest.Username).ConfigureAwait(false);
+            if (user == null)
+            {
+                return Unauthorized("Incorrect Username.");
+            }
+
+            if (!HashingUtil.VerifyPassword(resetPasswordRequest.OldPassword, user.HashedPassword, user.Salt))
+            {
+                return Unauthorized("Incorrect old password");
+            }
+
+            bool success = await _userRepository.UpdateUserPassword(user, resetPasswordRequest.NewPassword).ConfigureAwait(false);
+            if (!success)
+            {
+                return BadRequest("Something Went Wrong.");
+            }
+
+            return Ok("Password updated successfully.");
+        }
+    }
+}
---
+++ Models/ColleagueCastleModels/ResetPasswordRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace NewHorizon.Models.ColleagueCastleModels
+{
+    public class ResetPasswordRequest
+    {
+        public string Username { get; set; }
+
+        public string OldPassword { get; set; }
+
+        public string NewPassword { get; set; }
+    }
+}
